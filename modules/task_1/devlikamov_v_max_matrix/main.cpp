258049cbba091701e4a16894c474451f5d05650f
vlad <vlad.devlikamov@gmail.com>
2019-09-22 16:23:03 +0300

    fix

--- modules/task_1/devlikamov_v_max_matrix/main.cpp
+++ modules/task_1/devlikamov_v_max_matrix/main.cpp
@@ -3,9 +3,9 @@
 #include <gtest-mpi-listener.hpp>
 #include <gtest/gtest.h>
 #include <vector>
-#include "../../../modules/task_1/devlikamov_v_matrix_sum/matrix_sum.h"
+#include "../../../modules/task_1/devlikamov_v_max_matrix/max_matrix.h"
 
-TEST(Matrix_sum_MPI, test1_on_matrix) {
+TEST(Matrix_max_MPI, test1_on_matrix) {
     std::vector <std::vector<int>> a(2, std::vector <int> (2));
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -13,13 +13,13 @@ TEST(Matrix_sum_MPI, test1_on_matrix) {
     a[0][1] = 1;
     a[1][0] = 1;
     a[1][1] = -100;
-    int sum = SumOfMatrix(a, 2, 2);
+    int maxi = MaxOfMatrix(a, 2, 2);
     if (rank == 0) {
-        EXPECT_EQ(-96, sum);
+        EXPECT_EQ(2, maxi);
     }
 }
 
-TEST(Matrix_sum_MPI, test2_on_matrix) {
+TEST(Matrix_max_MPI, test2_on_matrix) {
     std::vector <std::vector<int>> a(3, std::vector <int> (3));
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -27,13 +27,13 @@ TEST(Matrix_sum_MPI, test2_on_matrix) {
     a[0][1] = 1;
     a[1][0] = 1;
     a[1][1] = -100;
-    int sum = SumOfMatrix(a, 3, 3);
+    int maxi = MaxOfMatrix(a, 3, 3);
     if (rank == 0) {
-        EXPECT_EQ(-96, sum);
+        EXPECT_EQ(2, maxi);
     }
 }
 
-TEST(Matrix_sum_MPI, test3_on_matrix) {
+TEST(Matrix_max_MPI, test3_on_matrix) {
     std::vector <std::vector<int>> a(3, std::vector <int> (3));
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -45,99 +45,119 @@ TEST(Matrix_sum_MPI, test3_on_matrix) {
     a[1][1] = -100;
     a[2][0] = -2;
     a[2][1] = -2;
-    int sum = SumOfMatrix(a, 3, 3);
+    int maxi = MaxOfMatrix(a, 3, 3);
     if (rank == 0) {
-        EXPECT_EQ(2, sum);
+        EXPECT_EQ(100, maxi);
     }
 }
 
-TEST(Matrix_sum_MPI, test_on_random_matrix1) {
+TEST(Matrix_max_MPI, test_on_random_matrix1) {
     std::vector <std::vector<int>> a = getRandomMatrixT(100, 100);
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    int sum = SumOfMatrix(a, 100, 100);
+    int maxi = MaxOfMatrix(a, 100, 100);
     if (rank == 0) {
-        EXPECT_EQ(0, sum);
+        EXPECT_EQ(1, maxi);
     }
 }
 
-TEST(Matrix_sum_MPI, test_on_random_matrix2) {
+TEST(Matrix_max_MPI, test_on_random_matrix2) {
     std::vector <std::vector<int>> a = getRandomMatrixT(100, 100);
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    int sum = SumOfMatrix(a, 100, 100);
+    int maxi = MaxOfMatrix(a, 100, 100);
     if (rank == 0) {
-        EXPECT_EQ(0, sum);
+        EXPECT_EQ(1, maxi);
     }
 }
 
-TEST(Matrix_sum_MPI, test_on_random_matrix3) {
+TEST(Matrix_max_MPI, test_on_random_matrix3) {
     std::vector <std::vector<int>> a = getRandomMatrixT(500, 500);
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    int sum = SumOfMatrix(a, 500, 500);
+    int maxi = MaxOfMatrix(a, 500, 500);
     if (rank == 0) {
-        EXPECT_EQ(0, sum);
+        EXPECT_EQ(1, maxi);
     }
 }
 
-TEST(Matrix_sum_MPI, test_on_random_matrix4) {
+TEST(Matrix_max_MPI, test_on_random_matrix4) {
     std::vector <std::vector<int>> a = getRandomMatrixT(100, 1);
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    int sum = SumOfMatrix(a, 100, 1);
+    int maxi = MaxOfMatrix(a, 100, 1);
     if (rank == 0) {
-        EXPECT_EQ(0, sum);
+        EXPECT_EQ(1, maxi);
     }
 }
 
-TEST(Matrix_sum_MPI, test_on_matrix_of_units_1) {
+TEST(Matrix_max_MPI, test_on_matrix_of_units_1) {
     std::vector <std::vector<int>> a = getRandomMatrix(100, 100);
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    int sum = SumOfMatrix(a, 100, 100);
+    int maxi = MaxOfMatrix(a, 100, 100);
     if (rank == 0) {
-        EXPECT_EQ(100*100, sum);
+        EXPECT_EQ(1, maxi);
     }
 }
 
-TEST(Matrix_sum_MPI, test_on_matrix_of_units_2) {
+TEST(Matrix_max_MPI, test_on_matrix_of_units_2) {
     std::vector <std::vector<int>> a = getRandomMatrix(50, 50);
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    int sum = SumOfMatrix(a, 50, 50);
+    int maxi = MaxOfMatrix(a, 50, 50);
     if (rank == 0) {
-        EXPECT_EQ(50*50, sum);
+        EXPECT_EQ(1, maxi);
     }
 }
 
-TEST(Matrix_sum_MPI, test_on_Identity_matrix_1) {
+TEST(Matrix_max_MPI, test_on_Identity_matrix_1) {
     std::vector <std::vector<int>> a = getRandomMatrixU(100, 100);
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    int sum = SumOfMatrix(a, 100, 100);
+    int maxi = MaxOfMatrix(a, 100, 100);
     if (rank == 0) {
-        EXPECT_EQ(100, sum);
+        EXPECT_EQ(1, maxi);
     }
 }
 
-TEST(Matrix_sum_MPI, test_on_Identity_matrix_2) {
+TEST(Matrix_max_MPI, test_on_Identity_matrix_2) {
     std::vector <std::vector<int>> a = getRandomMatrixU(50, 60);
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    int sum = SumOfMatrix(a, 50, 60);
+    int maxi = MaxOfMatrix(a, 50, 60);
     if (rank == 0) {
-        EXPECT_EQ(50, sum);
+        EXPECT_EQ(1, maxi);
     }
 }
 
-TEST(Matrix_sum_MPI, test_on_zero_matrix) {
+TEST(Matrix_max_MPI, test_on_zero_matrix) {
     std::vector <std::vector<int>> a(101, std::vector <int> (101));
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    int sum = SumOfMatrix(a, 101, 101);
+    int maxi = MaxOfMatrix(a, 101, 101);
     if (rank == 0) {
-        EXPECT_EQ(0, sum);
+        EXPECT_EQ(0, maxi);
+    }
+}
+
+TEST(Matrix_max_MPI, test_on_max_matrix1) {
+    std::vector <std::vector<int>> a = getRandomMatrixM(50, 60);
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    int maxi = MaxOfMatrix(a, 50, 60);
+    if (rank == 0) {
+        EXPECT_EQ(108, maxi);
+    }
+}
+
+TEST(Matrix_max_MPI, test_on_max_matrix2) {
+    std::vector <std::vector<int>> a = getRandomMatrixM(5, 60);
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    int maxi = MaxOfMatrix(a, 5, 60);
+    if (rank == 0) {
+        EXPECT_EQ(63, maxi);
     }
 }
 
--- modules/task_1/devlikamov_v_max_matrix/max_matrix.cpp
+++ modules/task_1/devlikamov_v_max_matrix/max_matrix.cpp
@@ -8,7 +8,7 @@
 #include <vector>
 #include <algorithm>
 #include <functional>
-#include "../../../modules/task_1/devlikamov_v_matrix_sum/matrix_sum.h"
+#include "../../../modules/task_1/devlikamov_v_max_matrix/max_matrix.h"
 
 std::vector<std::vector <int>> getRandomMatrix(int n, int m) {
     std::vector <std::vector <int>> Matrix(n, std::vector <int> (m));
@@ -38,7 +38,17 @@ std::vector<std::vector <int>> getRandomMatrixU(int n, int m) {
     return Matrix;
 }
 
-int SumOfMatrix(const std::vector <std::vector <int>> &a, int n, int m) {
+std::vector<std::vector <int>> getRandomMatrixM(int n, int m) {
+    std::vector <std::vector <int>> Matrix(n, std::vector <int> (m));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            Matrix[i][j] = i + j;
+        }
+    }
+    return Matrix;
+}
+
+int MaxOfMatrix(const std::vector <std::vector <int>> &a, int n, int m) {
     int answ = 0;
     int size, rank;
     MPI_Comm_size(MPI_COMM_WORLD, &size);
@@ -55,14 +65,14 @@ int SumOfMatrix(const std::vector <std::vector <int>> &a, int n, int m) {
     int ans = 0;
     if (rank == 0) {
         for (int i = 0; i < n; i += size) {
-            ans += std::accumulate(a[i].begin(), a[i].end(), 0);
+            ans = std::max(*max_element(a[i].begin(), a[i].end()), ans);
         }
     } else {
         for (int i = rank; i < n; i += size) {
             MPI_Recv(&b[0], m, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
-            ans += std::accumulate(b.begin(), b.end(), 0);
+            ans = std::max(ans, *max_element(b.begin(), b.end()));
         }
     }
-    MPI_Reduce(&ans, &answ, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    MPI_Reduce(&ans, &answ, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD);
     return answ;
 }
